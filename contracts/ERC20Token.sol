46185ef7cce0d3d6ef6fe0fed217dd1cbb3201d4
CorelVault <8863029c@sina.com>
2025-03-05 19:38:55 +0800

    Update ERC20Token.sol

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -8,19 +8,24 @@ contract ERC20Token is AccessControl {
     string public symbol;
     uint8 public decimals;
     uint256 public totalSupply;
-
+    
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _totalSupply) {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals,
+        uint256 _totalSupply
+    ) {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
-        totalSupply = _totalSupply;
-        balanceOf[msg.sender] = _totalSupply;
+        totalSupply = _totalSupply * (10 ** uint256(decimals));
+        balanceOf[msg.sender] = totalSupply;
     }
 
     function transfer(address recipient, uint256 amount) public returns (bool) {
@@ -38,11 +43,13 @@ contract ERC20Token is AccessControl {
     }
 
     function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
+        require(amount <= allowance[sender][msg.sender], "Allowance exceeded");
         require(balanceOf[sender] >= amount, "Insufficient balance");
-        require(allowance[sender][msg.sender] >= amount, "Allowance exceeded");
+        
+        allowance[sender][msg.sender] -= amount;
         balanceOf[sender] -= amount;
         balanceOf[recipient] += amount;
-        allowance[sender][msg.sender] -= amount;
+        
         emit Transfer(sender, recipient, amount);
         return true;
     }
