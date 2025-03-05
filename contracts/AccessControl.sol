af65afc7d953c9cea42cb0491c8d93e8dbeaec9c
CorelVault <8863029c@sina.com>
2025-03-05 19:28:56 +0800

    Update AccessControl.sol

--- contracts/AccessControl.sol
+++ contracts/AccessControl.sol
@@ -1,83 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IAccessControl.sol";
 
-contract AccessControl is Ownable {
-    mapping(address => bool) private operators;
-    mapping(address => bool) private admins;
+contract AccessControl is IAccessControl {
+    mapping(bytes32 => mapping(address => bool)) private _roles;
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
-    event AdminAdded(address indexed admin);
-    event AdminRemoved(address indexed admin);
-    event OperatorAdded(address indexed operator);
-    event OperatorRemoved(address indexed operator);
-
-    modifier onlyAdmin() {
-        require(admins[msg.sender] || msg.sender == owner(), "Not an admin");
-        _;
-    }
-
-    modifier onlyOperator() {
-        require(operators[msg.sender], "Not an operator");
+    modifier onlyRole(bytes32 role) {
+        require(_roles[role][msg.sender], "AccessControl: Unauthorized");
         _;
     }
 
     constructor() {
-        admins[msg.sender] = true;
-        emit AdminAdded(msg.sender);
+        _roles[ADMIN_ROLE][msg.sender] = true;
     }
 
-    function addAdmin(address _admin) external onlyOwner {
-        require(_admin != address(0), "Invalid address");
-        require(!admins[_admin], "Already an admin");
-        admins[_admin] = true;
-        emit AdminAdded(_admin);
+    function hasRole(bytes32 role, address account) external view override returns (bool) {
+        return _roles[role][account];
     }
 
-    function removeAdmin(address _admin) external onlyOwner {
-        require(admins[_admin], "Not an admin");
-        admins[_admin] = false;
-        emit AdminRemoved(_admin);
+    function grantRole(bytes32 role, address account) external override onlyRole(ADMIN_ROLE) {
+        _roles[role][account] = true;
+        emit RoleGranted(role, account, msg.sender);
     }
 
-    function addOperator(address _operator) external onlyAdmin {
-        require(_operator != address(0), "Invalid address");
-        require(!operators[_operator], "Already an operator");
-        operators[_operator] = true;
-        emit OperatorAdded(_operator);
-    }
-
-    function addOperators(address[] calldata _operators) external onlyAdmin {
-        for (uint256 i = 0; i < _operators.length; i++) {
-            address _operator = _operators[i];
-            if (_operator != address(0) && !operators[_operator]) {
-                operators[_operator] = true;
-                emit OperatorAdded(_operator);
-            }
-        }
-    }
-
-    function removeOperator(address _operator) external onlyAdmin {
-        require(operators[_operator], "Not an operator");
-        operators[_operator] = false;
-        emit OperatorRemoved(_operator);
-    }
-
-    function removeOperators(address[] calldata _operators) external onlyAdmin {
-        for (uint256 i = 0; i < _operators.length; i++) {
-            address _operator = _operators[i];
-            if (operators[_operator]) {
-                operators[_operator] = false;
-                emit OperatorRemoved(_operator);
-            }
-        }
-    }
-
-    function isAdmin(address _admin) external view returns (bool) {
-        return admins[_admin];
-    }
-
-    function isOperator(address _operator) external view returns (bool) {
-        return operators[_operator];
+    function revokeRole(bytes32 role, address account) external override onlyRole(ADMIN_ROLE) {
+        _roles[role][account] = false;
+        emit RoleRevoked(role, account, msg.sender);
     }
 }
