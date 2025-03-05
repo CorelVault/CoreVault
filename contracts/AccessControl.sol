4a840c97464913e3a70ee9fa9959e5433b91fac4
CorelVault <8863029c@sina.com>
2025-03-05 19:29:44 +0800

    Update AccessControl.sol

--- contracts/AccessControl.sol
+++ contracts/AccessControl.sol
@@ -5,27 +5,22 @@ import "./IAccessControl.sol";
 
 contract AccessControl is IAccessControl {
     mapping(bytes32 => mapping(address => bool)) private _roles;
-    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
-
+    
     modifier onlyRole(bytes32 role) {
-        require(_roles[role][msg.sender], "AccessControl: Unauthorized");
+        require(_roles[role][msg.sender], "AccessControl: not authorized");
         _;
-    }
-
-    constructor() {
-        _roles[ADMIN_ROLE][msg.sender] = true;
     }
 
     function hasRole(bytes32 role, address account) external view override returns (bool) {
         return _roles[role][account];
     }
 
-    function grantRole(bytes32 role, address account) external override onlyRole(ADMIN_ROLE) {
+    function grantRole(bytes32 role, address account) external override onlyRole(role) {
         _roles[role][account] = true;
         emit RoleGranted(role, account, msg.sender);
     }
 
-    function revokeRole(bytes32 role, address account) external override onlyRole(ADMIN_ROLE) {
+    function revokeRole(bytes32 role, address account) external override onlyRole(role) {
         _roles[role][account] = false;
         emit RoleRevoked(role, account, msg.sender);
     }
